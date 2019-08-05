e349fdcd989b86e43c19f1af14f81b36810b139c
azizyilmaz <azizxyilmaz@outlook.com>
2019-08-06 01:04:06 +0300

    Boxing&Unboxing, Implicit&Explicit Conversion

--- TypeConversionApp/MainProgram.cs
+++ TypeConversionApp/MainProgram.cs
@@ -6,20 +6,45 @@ using System.Threading.Tasks;
 
 namespace TypeConversionApp
 {
+    class CanliVarlik
+    {
+
+    }
+    class Insan : CanliVarlik
+    {
+
+    }
     class MainProgram
     {
         static void Main(string[] args)
         {
+            //Implicit & Explicit
+            CanliVarlik c = new Insan();
+            Insan i = (Insan)c;
+            //Boxing
+            double d = 797693134862;
+            object o;
+            o = d;
+            Console.WriteLine(d);
+            Console.WriteLine(o);
+            d++;
+            Console.WriteLine(d);
+            Console.WriteLine(o);
+            //Unboxing
+            double d2;
+            d2 = (double)o;
+            Console.WriteLine(d2);
+
             //int a = 12;
             //float b = 3.5F;
             //int c = a + Convert.ToInt32(b);
 
-            char l = 'k';
-            float b = 19.0f;
-            int c;
-            //c = (l / Convert.ToInt32(b));
-            c = Convert.ToInt32(l / b);
-            Console.WriteLine(c);
+            //char l = 'k';
+            //float b = 19.0f;
+            //int c;
+            ////c = (l / Convert.ToInt32(b));
+            //c = Convert.ToInt32(l / b);
+            //Console.WriteLine(c);
             Console.ReadKey();
         }
     }
