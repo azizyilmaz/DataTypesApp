d54a2fc63f82ac17b6b68e9ae65a2190679b99ad
azizyilmaz <azizxyilmaz@outlook.com>
2019-09-15 22:38:52 +0300

    Built-in Types

---
+++ csharp/BuiltinTypes/Conversion.cs
@@ -0,0 +1,17 @@
+﻿namespace BuiltinTypes
+{
+    public class Conversion
+    {
+        public static void Main(string[] args)
+        {
+            object left = (int)1;
+            object right = (int)1;
+            var comparison1 = left == right;
+            var comparison2 = left.Equals(right);
+            var comparison3 = (int)left == (int)right;
+
+            object boxedInt = (int)1;
+            long unboxedInt = (long)(int)boxedInt;
+        }
+    }
+}
